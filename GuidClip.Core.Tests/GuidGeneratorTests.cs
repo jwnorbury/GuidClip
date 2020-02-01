25db99c895cb58485fe847dc18ad5b34320ae058
James Norbury <jwnorbury@gmail.com>
2020-02-01 22:16:41 +0000

    Add ability to generate multiple guids.

--- GuidClip.Core.Tests/GuidGeneratorTests.cs
+++ GuidClip.Core.Tests/GuidGeneratorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace GuidClip.Core.Tests
@@ -10,6 +11,26 @@ namespace GuidClip.Core.Tests
         {
             var guidText = GuidGenerator.GenerateGuid();
             Assert.True(Guid.TryParse(guidText, out _));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void GenerateMultipleGuids_Valid(int numGuids)
+        {
+            var guidText = GuidGenerator.GenerateMultipleGuids(numGuids);
+            var guids = guidText.Split(Environment.NewLine);
+            var allGuidsValid = guids.All(x => Guid.TryParse(x, out _));
+            Assert.True(allGuidsValid);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-5)]
+        public void GenerateMultipleGuids_Zero_OutOfRange(int numGuids)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => GuidGenerator.GenerateMultipleGuids(numGuids));
         }
     }
 }
--- GuidClip.Core/GuidGenerator.cs
+++ GuidClip.Core/GuidGenerator.cs
@@ -1,17 +1,62 @@
 ﻿using System;
+using System.Text;
 
 namespace GuidClip.Core
 {
     /// <summary>
-    /// Class to generate GUIDs.
+    /// Class to generate GUIDs
     /// </summary>
     public class GuidGenerator
     {
+        const int GUID_LENGTH = 36;
+
+        private static int _newLineLength = int.MinValue;
+        private static int NewLineLength
+        {
+            get
+            {
+                if (_newLineLength == int.MinValue)
+                {
+                    _newLineLength = Environment.NewLine.Length;
+                }
+                return _newLineLength;
+            }
+        }
+
         /// <summary>
         /// Generate a single GUID.
         /// </summary>
         /// <returns>String of generated GUID.</returns>
         public static string GenerateGuid() =>
-            Guid.NewGuid().ToString();   
+            Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// Generate multiple GUIDs.
+        /// </summary>
+        /// <param name="numGuids">Number of GUIDs to generate.</param>
+        /// <returns>String of GUIDs separated by a new line.</returns>
+        public static string GenerateMultipleGuids(int numGuids)
+        {
+            if (numGuids <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "Number of GUIDs to generate must be greater that 0");
+            }
+
+            if (numGuids == 1)
+            {
+                return GenerateGuid();
+            }
+
+            var capacity = (numGuids * GUID_LENGTH)
+                + (numGuids * NewLineLength);
+            var sb = new StringBuilder(capacity);
+            for (var i = 0; i < numGuids; i++)
+            {
+                sb.AppendLine(GenerateGuid());
+            }
+
+            return sb.ToString(0, capacity - NewLineLength);
+        }
     }
 }
