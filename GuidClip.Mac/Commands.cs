4670eb0fb5f36ba0a2e7ab27316555d5402f4feb
James Norbury <jwnorbury@gmail.com>
2020-02-03 19:09:30 +0000

    Wrap GUIDs in single quotes to correctly pipe.

--- GuidClip.Mac/Commands.cs
+++ GuidClip.Mac/Commands.cs
@@ -8,7 +8,7 @@ namespace GuidClip.Mac
         public static void PbCopy(string text)
         {
             var shell = Environment.GetEnvironmentVariable("SHELL");
-            var processInfo = new ProcessStartInfo(shell, $"-c \"echo {text} | pbcopy\"");
+            var processInfo = new ProcessStartInfo(shell, $"-c \"echo '{text}' | pbcopy\"");
             processInfo.UseShellExecute = true;
             var process = new Process();
             process.StartInfo = processInfo;
