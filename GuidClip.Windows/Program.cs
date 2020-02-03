1afdd76e1462935a2f7b7992dede2f37d83a86b8
James Norbury <jwnorbury@gmail.com>
2020-02-03 10:48:27 +0000

    Add windows specific logic to copy to clipboard.

--- GuidClip.Windows/Program.cs
+++ GuidClip.Windows/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Windows;
+using GuidClip.Core;
 
 namespace GuidClip.Windows
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                SetSingleGuid();
+            }
+            else if (args.Length == 1)
+            {
+                if (!int.TryParse(args[0], out var numGuids))
+                {
+                    throw new FormatException("Expected argument to be an integer.");
+                }
+                else if (numGuids < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "If provided, argument must be greater than 0.");
+                }
+
+                SetMultipleGuids(numGuids);
+            }
+            else
+            {
+                throw new ArgumentException("Expected single integer or no arguments.");
+            }
+        }
+
+        private static void SetSingleGuid()
+        {
+            var guidText = GuidGenerator.GenerateGuid();
+            Clipboard.SetText(guidText);
+        }
+
+        private static void SetMultipleGuids(int numGuids)
+        {
+            var guidText = GuidGenerator.GenerateMultipleGuids(numGuids);
+            Clipboard.SetText(guidText);
         }
     }
 }
